09dc438e9ea01e6c367e7da32eba1af6034e1a7b
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2023-12-06 00:48:28 +0200

    chore: make a layout component

---
+++ application/server/resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1, viewport-fit=cover" />
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name') }}</title>
+
+  @vite(['resources/scss/index.scss', 'resources/js/index.js'])
+</head>
+
+<body>
+</body>
+
+</html>
