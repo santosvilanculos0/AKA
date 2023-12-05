9114f2835a0a68dd03d5b84a0bc0a355abcb5834
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2023-12-05 23:24:00 +0200

    chore: move sign in, sign up and forgot password view calls to their respective controllers

--- application/server/routes/auth.php
+++ application/server/routes/auth.php
@@ -12,27 +12,17 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    // Route::get('register', [RegisteredUserController::class, 'create'])
-    //     ->name('register');
-
-    Route::get('sign_up', fn() => view('auth.sign_up'))
+    Route::get('sign_up', [RegisteredUserController::class, 'create'])
         ->name('sign_up');
 
     Route::post('sign_up', [RegisteredUserController::class, 'store']);
 
-    // Route::get('login', [AuthenticatedSessionController::class, 'create'])
-    //     ->name('login');
-
-    Route::get('sign_in', fn() => view('auth.sign_in'))
+    Route::get('sign_in', [AuthenticatedSessionController::class, 'create'])
         ->name('sign_in');
-
 
     Route::post('sign_in', [AuthenticatedSessionController::class, 'store']);
 
-    // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-    //     ->name('password.request');
-
-    Route::get('forgot_password', fn() => view('auth.forgot_password'))
+    Route::get('forgot_password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
 
     Route::post('forgot_password', [PasswordResetLinkController::class, 'store'])
